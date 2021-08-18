fee2b2861efd7294a97657fb4a244306b1c2538e
Nirunfeng <2570034697@qq.com>
2021-08-18 22:10:29 +0800

    init

---
+++ src/main/java/com/yepass/YePassApplication.java
@@ -0,0 +1,13 @@
+package com.yepass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YePassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YePassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yepass/YePassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yepass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YePassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
