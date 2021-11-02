e484d0399f33bef472f83f39a146df7caa9ccaaf
Nirunfeng <2570034697@qq.com>
2021-11-02 23:43:00 +0800

    新增登录页面

---
+++ src/main/java/com/yepass/service/SubjectService.java
@@ -0,0 +1,12 @@
+package com.yepass.service;
+
+/**
+ * TODO
+ *
+ * @author Ni runfeng
+ * @description
+ * @email 2570034697@qq.com
+ * @date 2021/11/2 22:44
+ */
+public interface SubjectService {
+}
